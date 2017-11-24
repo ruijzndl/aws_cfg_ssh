fe578146ddfc1c9b154243c5b698694c10cda6ac
Danny Ruijzendaal <danny.ruijzendaal@philips.com>
2017-11-24 16:54:56 +0100

    tasc-initial-version: updated version to 0.5.4

--- Formula/formula.rb
+++ Formula/formula.rb
@@ -1,8 +1,8 @@
 class Notes < Formula
   desc "..."
   homepage "https://github.com/ruijzndl/tacs"
-  url "https://github.com/ruijzndl/tacs/archive/0.5.2.tarball.gz"
-  version "0.5.3"
+  url "https://github.com/ruijzndl/tacs/archive/0.5.4.tarball.gz"
+  version "0.5.4"
 
   def install
     bin.install "bin/tacs"
